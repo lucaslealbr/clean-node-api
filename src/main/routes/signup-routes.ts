8008e6ceffe77de88779b8da1537b5065120e205
Lucas Leal <lucasleal_br@hotmail.com>
2021-06-28 12:00:47 +0000

    feat: add LogMongoRepository to SignUpFactory

--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -3,9 +3,6 @@ import { adaptRoute } from '../adapters/express-route-adapter'
 import { makeSignUpController } from '../factories/signup'
 
 export default (router: Router): void => {
-  router.get('/test', (req, res) => {
-    res.send({ ok: 'ok' })
-  })
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
   router.post('/signup', adaptRoute(makeSignUpController()))
 }
