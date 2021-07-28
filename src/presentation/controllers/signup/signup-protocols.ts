80b1c40d61a0485c66dbf476d7da29de717001de
Lucas Leal <lucasleal_br@hotmail.com>
2021-07-28 23:44:56 +0000

    refactor: move Validation to protocols folder

--- src/presentation/controllers/signup/signup-protocols.ts
+++ src/presentation/controllers/signup/signup-protocols.ts
@@ -1,4 +1,4 @@
-export * from '../../helpers/validators/validation'
+export * from '../../protocols/validation'
 export * from '../../protocols'
 export * from '../../../domain/usecases/add-account'
 export * from '../../../domain/models/account'
