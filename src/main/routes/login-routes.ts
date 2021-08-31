507d8d4426ded2cc9e0bdc13fd229067a44de7fa
Lucas Leal <lucasleal_br@hotmail.com>
2021-08-31 15:29:08 +0000

    feat: add login route

--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,8 +1,10 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/express/express-route-adapter'
-import { makeSignUpController } from '../factories/signup/signup'
+import { makeLoginController } from '../factories/login/login-factory'
+import { makeSignUpController } from '../factories/signup/signup-factory'
 
 export default (router: Router): void => {
-  // eslint-disable-next-line @typescript-eslint/no-misused-promises
   router.post('/signup', adaptRoute(makeSignUpController()))
+  router.post('/login', adaptRoute(makeLoginController()))
 }
