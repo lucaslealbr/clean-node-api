2dde64ec916122d1c8bbc3b673a93bde6d7abadf
Lucas Leal <lucasleal_br@hotmail.com>
2021-09-07 12:34:03 +0000

    fix: ensure makeDbAddAccount with dependencies

--- src/main/factories/usecases/add-account/db-add-account-factory.ts
+++ src/main/factories/usecases/add-account/db-add-account-factory.ts
@@ -7,5 +7,5 @@ export const makeDbAddAccount = (): AddAccount => {
   const salt = 12
   const bcryptAdapter = new BcryptAdapter(salt)
   const accountMongoDb = new AccountMongoRepository()
-  return new DbAddAccount(bcryptAdapter, accountMongoDb)
+  return new DbAddAccount(bcryptAdapter, accountMongoDb, accountMongoDb)
 }
