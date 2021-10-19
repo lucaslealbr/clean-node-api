23147ab72bb9d30d0ecb7c25dfa091833bfb6361
Lucas Leal <lucasleal_br@hotmail.com>
2021-10-19 07:39:13 -0300

    feat: ensure add survey route returns 403 if no access token is provided

--- src/main/server.ts
+++ src/main/server.ts
@@ -4,6 +4,6 @@ import env from './config/env'
 MongoHelper.connect(env.mongoUrl)
   .then(async () => {
     const app = (await import('./config/app')).default
-    app.listen(env.port, () => console.log('Server running'))
+    app.listen(env.port, () => console.log('Server running !'))
   })
   .catch(console.error)
